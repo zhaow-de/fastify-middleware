6e706eefc0eb79f51f04b3adcba92deafeef0ac7
Zhao Wang <zhaow.km@gmail.com>
2022-04-22 01:11:25 +0200

    chore: bumped up npm dependencies

--- jest.config.js
+++ jest.config.js
@@ -19,10 +19,10 @@ module.exports = {
   coverageReporters: ['lcov', 'text', 'html'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 };
